32fd7efa201db677a702d40beed0a7c2bf0e7240
Holden-VanKeyes <102389369+Holden-VanKeyes@users.noreply.github.com>
2025-04-19 21:57:46 -0500

    fix knex

--- knexfile.ts
+++ knexfile.ts
@@ -26,7 +26,7 @@ const config: { [key: string]: Knex.Config } = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.POSTGRES_URL,
     pool: {
       min: 2,
       max: 10,
